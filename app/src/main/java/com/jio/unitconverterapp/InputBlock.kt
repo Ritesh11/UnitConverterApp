0ffc3e365d47d1c43d0666a4f55f2905c42af173
RITESH <riteshksingh76@gmail.com>
2024-08-10 19:02:48 +0530

    Added 2 way data flow

--- app/src/main/java/com/jio/unitconverterapp/InputBlock.kt
+++ app/src/main/java/com/jio/unitconverterapp/InputBlock.kt
@@ -1,20 +1,27 @@
 package com.jio.unitconverterapp
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -24,7 +31,9 @@ import androidx.compose.ui.unit.sp
 fun InputBlock(
     conversion: Conversion,
     inputText: MutableState<String>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    context: Context = LocalContext.current,
+    calculate: (String) -> Unit
 ) {
 
     Column(modifier = modifier.padding(0.dp, 20.dp, 0.dp, 0.dp)) {
@@ -54,6 +63,22 @@ fun InputBlock(
                     .padding(10.dp, 30.dp, 0.dp, 0.dp)
                     .fillMaxWidth(0.35F)
             )
+
+        }
+
+        Spacer(modifier = modifier.height(20.dp))
+        OutlinedButton(
+            onClick = {
+
+                calculate(inputText.value)
+           
+
+        }, modifier = modifier.fillMaxWidth(1F)
+        ) {
+            Text(text = "Convert",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue)
 
         }
 
